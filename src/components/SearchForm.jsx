8cca74d2df7060164ba5a77c0598c3269e85af87
Mohamed Idris <edriso.dev@gmail.com>
2026-01-23 17:14:51 +0200

    prepare search form with input handling and submission logic

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,32 @@
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // e.target === formRef.current
+    const searchValue = e.target.elements?.search?.value?.trim(); // Safely access the input value, even if the 'search' element is renamed or removed in the DOM
+    if (!searchValue) return;
+
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className='title'>Unsplash images</h1>
+
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='search'
+          placeholder='cat'
+          className='form-input search-input'
+        />
+
+        <button type='submit' className='btn'>
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
